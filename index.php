1cd32352dc120f56ee3fe701b662a071754205e7
rexia-intel-automation <mohamad@rexia.com.br>
2026-01-02 19:24:21 -0300

    Update index.php

--- index.php
+++ index.php
@@ -68,7 +68,7 @@
                             <path d="M9 12a4 4 0 1 0 4 4V4a5 5 0 0 0 5 5"/>
                         </svg>
                     </a>
-                    <a href="https://www.youtube.com/@CaixaBrasiliaBasquete" target="_blank" aria-label="YouTube">
+                    <a href="https://www.youtube.com/@brasilia.basquete" target="_blank" aria-label="YouTube">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17"/>
                             <path d="m10 15 5-3-5-3z"/>
